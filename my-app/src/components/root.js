13a6cc9cca5c53240883adad3e287173148b8039
geraldglynn <geraldglynn@gmail.com>
2021-03-13 10:02:43 +0000

    Added root

--- my-app/src/components/root.js
+++ my-app/src/components/root.js
@@ -1,12 +1,18 @@
 // import logo from './logo.svg';
 // import './App.css';
+import data from '../mock/reactjs.json'
+
+// console.log(data)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App1">
+      <header className="App-header1">
         <p>
           YO
+          { data.map(d =>{
+              console.log(d)
+          })}
         </p>
       </header>
     </div>
