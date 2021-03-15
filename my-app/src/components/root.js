7e3985c2a5410222d58eb70a1a2371135ce11c2b
geraldglynn <geraldglynn@gmail.com>
2021-03-15 13:59:39 +0000

    Created first basic component <Root />

--- my-app/src/components/root.js
+++ my-app/src/components/root.js
@@ -1,22 +1,84 @@
-// import logo from './logo.svg';
-// import './App.css';
-import data from '../mock/reactjs.json'
+import React, { useState, useEffect } from 'react'
+import { fetchSubreddit, fetchSubredditPosts } from '../api/reddit'
 
-// console.log(data)
-
-function App() {
-  return (
-    <div className="App1">
-      <header className="App-header1">
-        <p>
-          YO
-          { data.map(d =>{
-              console.log(d)
-          })}
-        </p>
-      </header>
-    </div>
-  );
+function decode(str){
+  return str.replace(/&#(\d+);/g, function(match, dec) {
+    return String.fromCharCode(dec);
+  });
 }
 
-export default App;
+function Item(props) {
+  const {
+    title,
+    text,
+  } = props
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{text}</p>
+
+
+    </>
+  )
+}
+
+function Subreddit() {
+  const [ postCount, setPostCount ] = useState([])
+  const [ posts, setPosts ] = useState([])
+  const topic = 'reactjs'
+
+  const getSubreddit = () => fetchSubreddit(topic).then(subreddit => setPosts(subreddit.children))
+
+  useEffect(getSubreddit, [])
+
+    return(
+      <>
+      { posts && posts.length > 0 && posts.map(
+        post => {
+          console.log(post)
+          const data = post.data || {}
+          const {
+            id,
+            archived,
+            author,
+            author_fullname,
+            category,
+            created_utc,
+            downs,
+            domain,
+            name,
+            num_comments,
+            over_18,
+            permalink,
+            pinned,
+            score,
+            selftext,
+            selftext_html,
+            subreddit,
+            subreddit_id,
+            subreddit_name_prefixed,
+            subreddit_subscribers,
+            subreddit_type,
+            ups,
+            upvote_ratio,
+            url,
+            wls,
+            title,
+          } = data
+
+          return(
+            <Item
+              key={id}
+              title={title}
+              text={decode(selftext)}
+            />
+          )
+        }
+      )}
+      </>
+    )
+}
+
+export default Subreddit
+
