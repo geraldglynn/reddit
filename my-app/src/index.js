3199a7e9dce293eb9aad13105274a1438c016d36
geraldglynn <geraldglynn@gmail.com>
2021-03-16 22:35:02 +0000

    Deleted root

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,8 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Root />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
