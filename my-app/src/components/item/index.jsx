6789da5cff9684bb535340ebaf9a8860f525a212
geraldglynn <geraldglynn@gmail.com>
2021-03-16 22:34:19 +0000

    Cleaned-up Item

--- my-app/src/components/item/index.jsx
+++ my-app/src/components/item/index.jsx
@@ -1,23 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
+import htmlEntity from 'he'
+import Modal from 'react-bootstrap/Modal'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import { format } from 'fecha';
+
+function isUrl(str){
+  return str.match(/^http/)
+}
 
 function Item(props) {
   const {
+    created_utc,
     title,
     text,
-    created_utc,
+    thumbnail,
   } = props
 
   const timestamp = new Date(created_utc * 1000)
   const formattedTimeDate = format(timestamp, 'h.mma [on] dddd MMMM Do, YYYY')
 
+  console.log({thumbnail})
+
+  const hasThumbnail = isUrl(thumbnail)
+
   return (
-    <>
-      <h1>{title}</h1>
+    <Col>
+      <h2>{htmlEntity.decode(title)}</h2>
       {formattedTimeDate}
-      <ReactMarkdown>{text}</ReactMarkdown>
-    </>
+      <Row>
+        { hasThumbnail &&
+        <Col xs={3}>
+          { <img src={thumbnail} /> }
+        </Col>
+        }
+        <Col xs={hasThumbnail ? 9 : 12}>
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </Col>
+      </Row>
+    </Col>
   )
 }
 
