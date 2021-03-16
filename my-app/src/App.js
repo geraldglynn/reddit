5b7036ad5630c32a500e4e318b8bab76fcf87b75
geraldglynn <geraldglynn@gmail.com>
2021-03-16 22:35:47 +0000

    Added routing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from 'react-router-dom'
+
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import 'style/main.scss'
+
+import Subreddit from 'components/subreddit'
+
+const topics = [
+  'reactjs',
+  'FreeCodeCamp',
+  'webdev',
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Row>
+        <Col xs={4}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+
+          <h2>Topics</h2>
+          <ul>
+            {topics.map(topic => 
+              <li>
+                <Link to={`/topics/${topic}`} disabled={true}>r/{topic}</Link>
+              </li>
+            )}
+          </ul>
+        </Col>
+        <Col xs={8}>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/topics">
+              <Topics />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          </Col>
+      </Row>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  let match = useRouteMatch()
+
+  return (
+    <div>
+      <Switch>
+        <Route path={`${match.path}/:topic/:page?`}>
+          <Subreddit />
+        </Route>
+        <Route path={match.path}>
+          <p>Please select a topic.</p>
+        </Route>
+      </Switch>
+    </div>
+  )
+}
+
+export default App
+
